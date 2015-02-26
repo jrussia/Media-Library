6e59d6fe58ac5ad7c4497d2034cfe51b239ff254
Jeremy Egner <jrussia@gmail.com>
2015-02-25 23:06:32 -0600

    DBController updates, syncing error

--- DBController.java
+++ DBController.java
@@ -17,63 +17,207 @@ import java.sql.ResultSet;
  * @version 0.0.1 2/23/2015
  */
 public class DBController {
-	
-	// Database db;	// if we need to store a reference to the database
-	
+
+	// Database db; // if we need to store a reference to the database
+
 	/**
 	 * Constructor
 	 */
 	public DBController(/* Database db */) {
-		//	this.db = db;
+		// this.db = db;
 	}
-	
+
 	/**
 	 * Add a movie to the database
 	 * 
-	 * @params	movie
-	 * 			conn
+	 * @params movie conn
 	 */
 	public static void addMovie(Movie movie, Connection conn) {
-		
+		// TODO: Verify that we need this
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		Statement stat;
+		// TODO: where is the best place to cache conn?
+		stat = conn.createStatement();
+		// TODO: We should escape these values
+		// TODO: Check the return value on this
+		stat.executeQuery("insert into MOVIE" + "values (" +
+				movie.getTitle() + "," + 
+				movie.getCover() + "," + 
+				movie.getYear() + "," + 
+				movie.getLength() + "," + 
+				movie.getPlot() + "," +
+				movie.getCast() + "," + 
+				movie.getAuthor() + "," +
+				movie.getGenre() + "," + 
+				movie.getLanguage() + "," +
+				movie.getCountry() + 
+				");"
+				);
 	}
-	
+
 	/**
 	 * Add a CD to the database
 	 * 
-	 * @params	cd
-	 * 			conn
+	 * @params cd conn
 	 */
 	public static void addCD(CD cd, Connection conn) {
-		
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		Statement stat;
+		// TODO: where is the best place to cache conn?
+		stat = conn.createStatement();
+		// TODO: We should escape these values
+		// TODO: Check the return value on this
+		stat.executeQuery("insert into CD" + "values (" +
+				CD.getTitle() + "," + 
+				CD.getCover() + "," + 
+				//CD.getYear() + "," + 
+				CD.getAuthor() + "," +
+				//movie.getGenre() + "," + 
+				CD.getISBN() +
+				");"
+				);
 	}
-	
+
 	/**
 	 * Add a book to the database
 	 * 
-	 * @params	book
-	 * 			conn
+	 * @params book conn
 	 */
 	public static void addBook(Book book, Connection conn) {
-		
+		// TODO: Verify that we need this
+				try {
+					Class.forName("org.sqlite.JDBC");
+				} catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+				Statement stat;
+				// TODO: where is the best place to cache conn?
+				stat = conn.createStatement();
+				// TODO: We should escape these values
+				// TODO: Check the return value on this
+				stat.executeQuery("insert into MOVIE values (" +
+						book.getTitle() + "," + 
+						book.getCover() + "," + 
+						book.getYear() + "," + 
+						book.getLength() + "," + 
+						book.getPlot() + "," + 
+						book.getAuthor() + "," + 
+						book.getISBN() + "," +
+						");"
+						);
 	}
-	
+
 	/**
 	 * Update media in the database
 	 * 
-	 * @params	media
-	 * 			conn
+	 * @params media conn
 	 */
 	public static void update(Media media, Connection conn) {
-		
+		// TODO: Verify that we need this
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		Statement stat;
+		// TODO: where is the best place to cache conn?
+		stat = conn.createStatement();
+		// TODO: We should escape these values
+		// TODO: Check the return value on this
+		if (media instanceof Book) {
+			// TODO: I don't think this is the right way to generalize this
+			media = (Book) media;
+			stat.executeQuery("update BOOK set" +
+					"BOOK_ISBN = " + media.getISBN() + "," +
+					"BOOK_TITLE = " + media.getTitle() + "," +
+					"BOOK_YEAR = " + media.getYear() + "," +
+					"BOOK_PLOT = " + media.getPlot() + "," +
+					"BOOK_LENGTH_PAGES = " + media.getLength() + "," +
+					"BOOK_COVER_FILEPATH = " + media.getCover() + "," +
+					"BOOK_AUTHOR_ID = " + media.getAuthor() +
+					"where BOOK_ID = " + media.getId() +
+					";"
+			);
+		}
+		if (media instanceof CD) {
+			// TODO: We should escape these values
+			// TODO: Check the return value on this
+				// TODO: I don't think this is the right way to generalize this
+				media = (CD) media;
+				stat.executeQuery("update CD set" +
+						"CD_ISBN = " + media.getISBN() + "," +
+						"CD_TITLE = " + media.getTitle() + "," +
+						"CD_COVER = " + media.getCover() + "," +
+						"CD_AUTHOR_ID = " + media.getAuthor() +
+						"where CD_ID = " + media.getId() +
+						";"
+						);
+		}
+			
+		if (media instanceof Movie) {
+			// TODO: We should escape these values
+			// TODO: Check the return value on this
+			if (media instanceof Movie) {
+				// TODO: I don't think this is the right way to generalize this
+				media = (Movie) media;
+				stat.executeQuery("update MOVIE set" +
+						"MOVIE_ISBN = " + media.getISBN() + "," +
+						"MOVIE_TITLE = " + media.getTitle() + "," +
+						"MOVIE_YEAR = " + media.getYear() + "," +
+						"MOVIE_PLOT = " + media.getPlot() + "," +
+						"MOVIE_CAST = " + media.getCast() + "," +
+						"MOVIE_LENGTH_MINUTES = " + media.getLength() + "," +
+						"MOVIE_COVER_FILEPATH = " + media.getCover() + "," +
+						"MOVIE_AUTHOR_ID = " + media.getAuthor() +
+						"MOVIE_GENRE_ID = " + media.getGenre() +
+						"MOVIE_LANGUAGE_ID = " + media.getLanguage() +
+						"MOVIE_COUNTRY_ID = " + media.getCountry() +
+						"where MOVIE_ID = " + media.getId() +
+						";"
+						);					
+			}
+		}
 	}
-	
+
 	/**
 	 * Delete media from the database
 	 * 
-	 * @params	media
-	 * 			conn
+	 * @params media conn
 	 */
 	public static void delete(Media media, Connection conn) {
-		
+		// TODO: Verify that we need this
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		Statement stat;
+		// TODO: where is the best place to cache conn?
+		stat = conn.createStatement();
+		// TODO: We should escape these values
+		// TODO: Check the return value on this
+		if (media instanceof Book) {
+			media = (Book) media;
+			stat.executeQuery("delete from BOOK" +
+					"where BOOK_ID = " + media.getId();
+		}
+		if (media instanceof CD) {
+			media = (CD) media;
+			stat.executeQuery("delete from CD" +
+					"where CD_ID = " + media.getId();
+		}
+		if (media instanceof Movie) {
+			media = (Movie) media;
+			stat.executeQuery("delete from MOVIE" +
+					"where MOVIE_ID = " + media.getId();
+		}
 	}
 }
