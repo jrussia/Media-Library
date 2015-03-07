7bffe0ab8dedd6ad33ae9c18353ca53213771a84
kstisser <fm8733@gmail.com>
2015-03-06 22:41:50 -0800

    Image Processing class

---
+++ imageProcessing.java
@@ -0,0 +1,28 @@
+package media;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class imageProcessing {
+
+	public String readBarcode (Image image){
+		
+		return null;
+	}
+	
+	public String OCR (Image image){
+		
+		return null;
+	}
+	
+	public int compareImages (Image image, Image image2){
+		
+		return 0;
+	}
+	
+}
