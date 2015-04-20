6e631e17789282d4c0f46059d68065ac303a21fd
kstisser <fm8733@gmail.com>
2015-04-19 19:35:04 -0700

    Forgot to update TakePicture
    Forgot this on the last update

--- TakePicture.java
+++ TakePicture.java
@@ -1,29 +1,57 @@
+package media;
+
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.*;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamPicker;
+import com.github.sarxos.webcam.WebcamResolution;
 
 
 public class TakePicture {
-
+	public static Webcam webcam = null;
+	public static BufferedImage img;
+	public static JFrame frame;
+	public static JButton captureBtn;
+	public static void showWebcam(){
+		img = null;
+		webcam = Webcam.getDefault();
+		WebcamPicker picker = new WebcamPicker();
 	
-	public TakePicture() throws IOException {
+		webcam = picker.getSelectedWebcam();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setImageSizeDisplayed(true);
+		//panel.setMirrored(false);
+		panel.setLayout(new BorderLayout());
 		
-		Webcam webcam = Webcam.getDefault();
+		captureBtn = new JButton("Capture");
+		panel.add(captureBtn, BorderLayout.SOUTH);
 		
-		try {	//set a fairly standard custom resolution
-			webcam.setViewSize(new Dimension(640,480));
-		} catch (IllegalArgumentException e) {}
+		frame = new JFrame("Take Photo");
+		frame.add(panel);
+		frame.setResizable(true);
+		frame.setTitle("Webcam Take Picture");
+		frame.pack();
+		frame.setVisible(true);
 		
-		webcam.open();
-		
-		BufferedImage image = webcam.getImage();
-		ImageIO.write(image, "JPG", new File("cachePix.jpg"));
-		webcam.close();
+		if(webcam == null){
+			JFrame parent = new JFrame();
+        	JOptionPane.showMessageDialog(parent, "There was a problem connecting to your webcam.", "Alert", JOptionPane.ERROR_MESSAGE);
+		}
+		return;
 	}
-
 }
